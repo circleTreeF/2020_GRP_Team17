79097fc1c6510e693dd76150a97a9f836744c902
Yizirui Fang <circletree@outlook.com>
2021-02-28 23:01:54 +0800

    feat(model): add app model to detect the bad points from the records collected from the mobile app

---
+++ model/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+# Create your models here.
+
+class NormalRoadTemplate(models.Model):
+    round_log = models.JSONField('Record Log File')
+
+
+class AbnormalRoadTemplate(models.Model):
+    round_log = models.JSONField('Record Log File')
+
+
+# The templates of bad points to calibrate in K-Mean step of detection
+class BadPointsTemplates(models.Model):
+    point_longitude = models.FloatField('Longitude of Point')
+    point_latitude = models.FloatField('Latitude of Point')
+    point_radius = models.FloatField('Radius of Point')
