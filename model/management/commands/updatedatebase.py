758b21a29e87b8a78f92475b6c45c0a9ffd6a064
Yizirui Fang <circletree@outlook.com>
2021-03-03 23:33:58 +0800

    perf(cron task): schedule to update the bad points table in the database powered with package django_apscheduler instead of schedule

---
+++ model/management/commands/updatedatebase.py
@@ -0,0 +1,61 @@
+import logging
+
+from django.conf import settings
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.cron import CronTrigger
+from django.core.management.base import BaseCommand
+from django_apscheduler.jobstores import DjangoJobStore
+from django_apscheduler.models import DjangoJobExecution
+# from model.engine.detect import update
+
+logger = logging.getLogger(__name__)
+
+
+def my_job():
+    # update()
+    print("Hello world")
+    pass
+
+
+def delete_old_job_executions(max_age=604_800):
+    """This job deletes all apscheduler job executions older than `max_age` from the database."""
+    DjangoJobExecution.objects.delete_old_job_executions(max_age)
+
+
+class Command(BaseCommand):
+    help = "Runs apscheduler to update the database about the bad points for viewing the abnormal road condition"
+
+    def handle(self, *args, **options):
+        scheduler = BlockingScheduler(timezone=settings.TIME_ZONE)
+        scheduler.add_jobstore(DjangoJobStore(), "default")
+
+        scheduler.add_job(
+            my_job,
+            trigger=CronTrigger(hour="23", minute="02"),  # Midnight everyday
+            id="my_job",  # The `id` assigned to each job MUST be unique
+            max_instances=1,
+            replace_existing=True,
+        )
+        logger.info("Added job 'my_job'.")
+
+        scheduler.add_job(
+            delete_old_job_executions,
+            trigger=CronTrigger(
+                hour="00", minute="00"
+            ),  # Midnight every day
+            id="delete_old_job_executions",
+            max_instances=1,
+            replace_existing=True,
+        )
+        logger.info(
+            "Added weekly job: 'delete_old_job_executions'."
+        )
+
+        try:
+            logger.info("Starting scheduler...")
+            scheduler.start()
+        except KeyboardInterrupt:
+            logger.info("Stopping scheduler...")
+            scheduler.shutdown()
+            logger.info("Scheduler shut down successfully!")
