314746e044cf62ad8fb3fa2175f8cab80bb6522d
Yizirui Fang <circletree@outlook.com>
2021-03-03 22:45:47 +0800

    feat(auto execution): add the scheduler for the detection with package schedule

--- model/engine/databaseUpdate.py
+++ model/engine/databaseUpdate.py
@@ -1,5 +1,14 @@
-import detect
+# import detect
 import schedule
-import time
+import sched, time
 
+def job():
+    print("Hello world")
+# job = detect.update()
+# schedule.every(10).minutes.do(job)
+# schedule.every().hour.do(job)
+schedule.every().day.at("10:00").do(job)
 
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
