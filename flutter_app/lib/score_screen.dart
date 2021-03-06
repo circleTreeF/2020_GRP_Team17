072a6a7b312545271a487b2dd7662037c4412df5
scysh1 <scysh1@nottingham.edu.cn>
2021-03-06 17:03:00 +0800

    UI(login/register) : change UI ; the app now can run on ios.

--- flutter_app/lib/score_screen.dart
+++ flutter_app/lib/score_screen.dart
@@ -29,8 +29,6 @@ class _ScoreState extends State<Score> {
     mainAxisSize: MainAxisSize.min,
     crossAxisAlignment: CrossAxisAlignment.start,
     children: <Widget>[
-    // Text(drivingGrade().toString()),
-      //Text("score"),
       sendData(mapGenerator())
     ],
     ),
@@ -38,56 +36,8 @@ class _ScoreState extends State<Score> {
   }
 
 
-  //score method
-
-  // String drivingGrade(){
-  //   List _targetList=widget.list;
-  //
-  //   double varianceX = 0;
-  //   double varianceY = 0;
-  //
-  //   const double standardX = 0; // threshold in X axis, needs to be calculated
-  //   const double standardY = 0; // threshold in Y axis, needs to be calculated
-  //
-  //   varianceX = varianceOfList(_targetList, "x");
-  //   varianceY = varianceOfList(_targetList, "Z");//??
-  //
-  //   if (varianceX <= standardX){
-  //     if(varianceY <= standardY){
-  //       mark = 'D' ;
-  //     }else{
-  //       mark = 'C' ;
-  //     }
-  //   }else if(varianceY <= standardY){
-  //     mark = 'B' ;
-  //   }else{
-  //     mark ='A' ;
-  //   }
-  //
-  //   return mark;
-  // }
-
-
-  // double varianceOfList(List _targetList, String key){
-  //   double varianceOfList = 0;
-  //   List<double> numberList = [];
-  //
-  //   int counter = 0;
-  //
-  //   for(counter = 0; counter < _targetList.length; counter++){
-  //     numberList.add(_targetList[counter]["key"]);
-  //   }
-  //   Array numberArray = Array(numberList);
-  //   varianceOfList = variance(numberArray);
-  //
-  //   //print(varianceOfList);
-  //
-  //   return varianceOfList;
-  // }
-
- Data mapGenerator() {
+  Data mapGenerator() {
     return map = new Data(UserAccount().user_id,widget.list,mark);
-
   }
 
   Text sendData(Data data) {
@@ -100,7 +50,6 @@ class _ScoreState extends State<Score> {
     createPost2(putData).then((response) {
       if (response.statusCode >= 200)
         print(response.body);
-      //else
     } // print(response.statusCode);
      ).catchError((error){
       print('error : $error');
