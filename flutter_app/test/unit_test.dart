022f73c2c64a8d604166acc0af8cb245892333e2
Daze_2 <scyhp1@nottingham.edu.cn>
2021-03-16 15:42:16 +0800

    test(unit_test): unit test finished

--- flutter_app/test/unit_test.dart
+++ flutter_app/test/unit_test.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/database/controller/post_service.dart';
 import 'package:flutter_app/driving/controller/driving_data_process.dart';
+import 'package:flutter_app/driving/driving_data_view.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
@@ -321,71 +323,141 @@ void main() {
 
     test('drivingGrade test 2', () {
       _testList = [
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0}
+        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 1, 'y': 2.9, 'z': 0},
+        {
+          'time': 0,
+          'latitude': 90,
+          'longitude': 90,
+          'x': 1.78,
+          'y': 1.64,
+          'z': 0
+        },
+        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 2, 'y': -0.8, 'z': 0},
+        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 2.5, 'y': 1, 'z': 0},
+        {
+          'time': 0,
+          'latitude': 90,
+          'longitude': 90,
+          'x': 1.9,
+          'y': -1.3,
+          'z': 0
+        },
+        {
+          'time': 0,
+          'latitude': 90,
+          'longitude': 90,
+          'x': 1.8,
+          'y': 1.6,
+          'z': 0
+        },
+        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 3.0, 'y': -2, 'z': 0},
+        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 3.6, 'y': 1, 'z': 0},
+        {
+          'time': 0,
+          'latitude': 90,
+          'longitude': 90,
+          'x': -2.0,
+          'y': -0.9,
+          'z': 0
+        },
+        {
+          'time': 0,
+          'latitude': 90,
+          'longitude': 90,
+          'x': 1.2,
+          'y': 0.1,
+          'z': 0
+        },
+        {
+          'time': 0,
+          'latitude': 90,
+          'longitude': 90,
+          'x': -0.9,
+          'y': -0.6,
+          'z': 0
+        },
+        {
+          'time': 0,
+          'latitude': 90,
+          'longitude': 90,
+          'x': 1.78,
+          'y': 0.8,
+          'z': 0
+        },
+        {
+          'time': 0,
+          'latitude': 90,
+          'longitude': 90,
+          'x': 1.92,
+          'y': 1.0,
+          'z': 0
+        },
+        {
+          'time': 0,
+          'latitude': 90,
+          'longitude': 90,
+          'x': 1.3,
+          'y': 1.2,
+          'z': 0
+        },
+        {
+          'time': 0,
+          'latitude': 90,
+          'longitude': 90,
+          'x': 0.9,
+          'y': -2.5,
+          'z': 0
+        }
+      ];
+
+      mark = testClass.drivingGrade(_testList);
+      expect(mark, 1);
+    });
+
+    test('drivingGrade test 3', () {
+      _testList = [
+        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 2.9, 'z': 0},
+        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 1.64, 'z': 0},
+        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': -0.8, 'z': 0},
+        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 1, 'z': 0},
+        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': -1.3, 'z': 0},
+        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 1.6, 'z': 0},
+        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': -2, 'z': 0},
+        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 1, 'z': 0},
+        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': -0.9, 'z': 0},
+        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0.1, 'z': 0},
+        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': -0.6, 'z': 0},
+        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0.8, 'z': 0},
+        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 1.0, 'z': 0},
+        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 1.2, 'z': 0},
+        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': -2.5, 'z': 0}
       ];
 
       mark = testClass.drivingGrade(_testList);
       expect(mark, 3);
     });
 
-    test('drivingGrade test 3', () {
+    test('drivingGrade test 4', () {
       _testList = [
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0}
+        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 1, 'y': 0, 'z': 0},
+        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 1.78, 'y': 0, 'z': 0},
+        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 2, 'y': 0, 'z': 0},
+        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 2.5, 'y': 0, 'z': 0},
+        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 1.9, 'y': 0, 'z': 0},
+        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 1.8, 'y': 0, 'z': 0},
+        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 3.0, 'y': 0, 'z': 0},
+        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 3.6, 'y': 0, 'z': 0},
+        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': -2.0, 'y': 0, 'z': 0},
+        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 1.2, 'y': 0, 'z': 0},
+        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': -0.9, 'y': 0, 'z': 0},
+        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 1.78, 'y': 0, 'z': 0},
+        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 1.92, 'y': 0, 'z': 0},
+        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 1.3, 'y': 0, 'z': 0},
+        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0.9, 'y': 0, 'z': 0}
       ];
 
       mark = testClass.drivingGrade(_testList);
       expect(mark, 2);
-    });
-
-    test('drivingGrade test 4', () {
-      _testList = [
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0},
-        {'time': 0, 'latitude': 90, 'longitude': 90, 'x': 0, 'y': 0, 'z': 0}
-      ];
-
-      mark = testClass.drivingGrade(_testList);
-      expect(mark, 1);
     });
 
     test('varianceOfList test 1', () {
@@ -432,16 +504,6 @@ void main() {
 
       testVariance = testClass.varianceOfList(_testList, "x");
       expect(testVariance, 0.4435838095238093);
-    });
-  });
-
-  group('driving data view', () {
-    test('GPS test',(){
-
-    });
-
-    test('Acc test',(){
-
     });
   });
 }
