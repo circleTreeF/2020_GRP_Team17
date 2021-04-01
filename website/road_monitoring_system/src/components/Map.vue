991850d7d2d291381cd8955a08c97104d7603fcb
scysq1 <scysq1@nottingham.edu.cn>
2021-04-01 19:41:30 +0800

    style:remove useless commandds

--- website/road_monitoring_system/src/components/Map.vue
+++ website/road_monitoring_system/src/components/Map.vue
@@ -83,7 +83,7 @@ export default {
       url: "http://10.6.2.61:8866/statistics/get/bad/points?date=",
       amapManager,
       zoom: 17,
-      language: "en",
+      // language: "en",
       center: [121.562236, 29.8000444],
       date:'',
       badPoints: [],
